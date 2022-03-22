9373e4414919307eeddf3a32b0bbcf14568730be
Maël Charpentier <55336346+MaelC001@users.noreply.github.com>
2022-03-22 14:13:07 -0400

    download fattext model

--- fattext_download.py
+++ fattext_download.py
@@ -1,7 +1,7 @@
 import fasttext
 import fasttext.util
 fasttext.util.download_model('es', if_exists='ignore')  # Espanish
-ft = fasttext.load_model('cc.es.100.bin')
+ft = fasttext.load_model('cc.es.300.bin')
 
 print(ft.get_dimension())
 fasttext.util.reduce_model(ft, 100)
